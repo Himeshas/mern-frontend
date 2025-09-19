4468fd97ca6d5209463e18196f81e1f9b3501cbe
Himeshas <sandaruwan519@gmail.com>
2025-09-20 00:00:38 +0530

    git origin

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ProductCard from './components/productCard.jsx'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <ProductCard/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/productCard.jsx
@@ -0,0 +1,9 @@
+export default function ProductCard(){
+
+    return(
+        <div>
+            <h1>Hello Himesha</h1>
+        </div>
+    )
+
+}
