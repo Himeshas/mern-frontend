c8aebb3f03e2c6ccfcf365f3043c95447b5d10bc
Himeshas <sandaruwan519@gmail.com>
2025-09-20 21:06:01 +0530

    possisions

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <div className="w-full h-screen bg-amber-400 flex flex-col justify-center items-center">
-        <div className='w-[75px] h-[45px] bg-red-900'></div> 
-               <FeaturedProduct/>
+        <div className='w-[75px] h-[45px] bg-red-900 fixed right-[100px] top-[100px] '></div> 
+               <FeaturedProduct />
         <ProductCard name="Mountain" price = "800.00" image = "https://picsum.photos/seed/picsum/200/300"/>
         <ProductCard name="Unknown" price = "550.00" image = "https://picsum.photos/id/20/200/300"/>
       </div>
--- src/components/featuredProduct.jsx
+++ src/components/featuredProduct.jsx
@@ -4,7 +4,7 @@ import ProductCard from './productCard'
 export default function FeaturedProduct() {
   return (
     <div>
-      <h1>Feature this week..!!!!</h1>
+      <h1 className='bg-purple-900 absolute left-[200px] top-[500px]'>Feature this week..!!!!</h1>
       <ProductCard
        name = "NFS"
        image = "https://picsum.photos/id/18/200/300"
