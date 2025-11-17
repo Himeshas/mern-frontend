09b53ea901a7c2d16b500303d7e852893fbbcac5
Himeshas <sandaruwan519@gmail.com>
2025-11-17 18:47:48 +0530

    Navigation part 01

--- src/pages/adminpage.jsx
+++ src/pages/adminpage.jsx
@@ -1,13 +1,18 @@
-import { Route , Routes } from "react-router-dom";
+import {Link, Route , Routes } from "react-router-dom";
 
 export default function AdminPage() {
   return (
     <div className="w-full h-screen bg-red-600 flex">
-        <div className="w-[300px] h-full bg-amber-50">
+        <div className="w-[300px] h-full flex flex-col">
+          <span className="text-3xl font-bold my-5">Admin Panel</span>
 
+          <Link to="/admin/products">Products</Link>
+          <Link to="/admin/orders">Orders</Link>
+          <Link to="/admin/users">Users</Link>
+          <Link to="/admin/settings">Settings</Link>
         </div>
 
-        <div className="w-[calc(100%-300px)] bg-blue-950 h-full">
+        <div className="w-[calc(100%-300px)] h-full">
 
           <Routes path="/*">
             <Route path="/" element={<h1>Dashboard</h1>} />
