5b8b4c8b5fc14dd6bf6d2673879dc8da8f5373c8
Himeshas <sandaruwan519@gmail.com>
2025-11-20 15:04:18 +0530

    Login page create part one.

--- src/pages/adminpage.jsx
+++ src/pages/adminpage.jsx
@@ -1,15 +1,18 @@
 import {Link, Route , Routes } from "react-router-dom";
+import { FaProductHunt } from "react-icons/fa6";
+import { CiShoppingCart } from "react-icons/ci";
+import { FaRegUser } from "react-icons/fa";
+import { IoIosSettings } from "react-icons/io";
 
 export default function AdminPage() {
   return (
     <div className="w-full h-screen bg-red-600 flex">
         <div className="w-[300px] h-full flex flex-col">
           <span className="text-3xl font-bold my-5">Admin Panel</span>
-
-          <Link to="/admin/products">Products</Link>
-          <Link to="/admin/orders">Orders</Link>
-          <Link to="/admin/users">Users</Link>
-          <Link to="/admin/settings">Settings</Link>
+          <Link className="flex flex-row h-[60px] w-full p-[20px] items-center gap-[10px]" to="/admin/products"><FaProductHunt />Products</Link>
+          <Link className="flex flex-row h-[60px] w-full p-[20px] items-center gap-[10px]" to="/admin/orders"><CiShoppingCart />Orders</Link>
+          <Link className="flex flex-row h-[60px] w-full p-[20px] items-center gap-[10px]" to="/admin/users"><FaRegUser />Users</Link>
+          <Link className="flex flex-row h-[60px] w-full p-[20px] items-center gap-[10px]" to="/admin/settings"><IoIosSettings />Settings</Link>
         </div>
 
         <div className="w-[calc(100%-300px)] h-full">
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function LoginPage() {
   return (
-    <div>
+    <div className='w-full h-screen bg-[url(./background.jpg)] bg-cover bg-center flex justify-center items-center'>
 
-        <span>This is login page</span>
+      <div className='w-[500px] h-[500px] backdrop-blur-sm rounded-[30px] shadow-2xl text-white flex flex-col items-center justify-center relative gap-[20px]'>
+        <h1 className='absolute top-[20px] text-2xl font-bold text-center my-5'>Log In</h1>
+        <div className='w-[350px] flex flex-col  '>
+          <span className='text-lg  '>Email</span>
+          <input type="text" className='w-[350px] h-[60px] border border-white rounded-xl focus:border-blue-600 focus:outline-none text-black'/>
+        </div>
+        <div className='w-[350px] flex flex-col  '>
+          <span className='text-lg  '>Password</span>
+          <input type="password" className='w-[350px] h-[60px] border border-white rounded-xl focus:border-blue-600 focus:outline-none text-black'/>
+        </div>
+
+        <button className='w-[350px] h-[40px] bg-blue-700 rounded-xl text-white text-lg mt-5 hover:bg-blue-500 transition-all duration-300'>Login</button>
+        <p>Don't have an account? <Link to='/register' className='text-blue-500'>Sign Up</Link> from here.</p>
+      </div>
+        
       
     </div>
   )
