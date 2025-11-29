fb55e56cab5ec3736ef1ba9761c99516faa42414
Himeshas <sandaruwan519@gmail.com>
2025-11-29 20:17:06 +0530

    onChange event

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function LoginPage() {
+  const[password, setPassword]= useState("") // Repeate
+  const[email, setEmail]= useState("")
+
+  function login(){
+    console.log(email)
+    console.log(password)
+  }
   return (
     <div className='w-full h-screen bg-[url(./background.jpg)] bg-cover bg-center flex justify-center items-center'>
 
@@ -9,14 +16,18 @@ export default function LoginPage() {
         <h1 className='absolute top-[20px] text-2xl font-bold text-center my-5'>Log In</h1>
         <div className='w-[350px] flex flex-col  '>
           <span className='text-lg  '>Email</span>
-          <input type="text" className='w-[350px] h-[60px] border border-white rounded-xl focus:border-blue-600 focus:outline-none text-black'/>
+          <input onChange={(e)=>{
+            setEmail(e.target.value)
+          }} type="text" className='w-[350px] h-[60px] border border-white rounded-xl focus:border-blue-600 focus:outline-none text-black'/>
         </div>
         <div className='w-[350px] flex flex-col  '>
           <span className='text-lg  '>Password</span>
-          <input type="password" className='w-[350px] h-[60px] border border-white rounded-xl focus:border-blue-600 focus:outline-none text-black'/>
+          <input onChange={(e)=>{
+            setPassword(e.target.value)
+            }} type="password" className='w-[350px] h-[60px] border border-white rounded-xl focus:border-blue-600 focus:outline-none text-black'/>
         </div>
 
-        <button className='w-[350px] h-[40px] bg-blue-700 rounded-xl text-white text-lg mt-5 hover:bg-blue-500 transition-all duration-300'>Login</button>
+        <button onClick={login} className='w-[350px] h-[40px] bg-blue-700 rounded-xl text-white text-lg mt-5 hover:bg-blue-500 transition-all duration-300'>Login</button>
         <p>Don't have an account? <Link to='/register' className='text-blue-500'>Sign Up</Link> from here.</p>
       </div>
         
